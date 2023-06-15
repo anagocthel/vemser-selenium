8ef81eff55e84d3c1a644281a9d1fd572bb9dc29
Ana Vitória Göcthel Silveira <ana.gocthel@dbccompany.com.br>
2023-06-15 11:19:12 -0300

    correção de anotacoes

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,25 +2,27 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import pages.LoginPage;
 
 public class BaseTest {
     private WebDriver driver;
     protected LoginPage loginPage;
 
-    public void setUp(){
+   @BeforeClass
+    public void setUp() {
         System.setProperty("wobdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
 
         loginPage = new LoginPage(driver);
 
+    }
+
+    @AfterClass
+    public void tearDown(){
+
         driver.quit();
     }
-    public static void main(String args[]){
-        BaseTest test = new BaseTest();
-        test.setUp();
-    }
-
-
 }
--- src/test/java/login/LoginTests.java
+++ src/test/java/login/LoginTests.java
@@ -1,24 +1,14 @@
 package login;
 
 import base.BaseTest;
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 import pages.InventoryPage;
-import pages.LoginPage;
 
 public class LoginTests extends BaseTest {
-    private WebDriver driver;
-    protected LoginPage loginPage;
 
     @Test
     public void testSuccessfulLogin() {
-        System.setProperty("wobdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-
-        loginPage = new LoginPage(driver);
 
         loginPage.setUsername("standard_user");
         loginPage.setPassword("secret_sauce");
